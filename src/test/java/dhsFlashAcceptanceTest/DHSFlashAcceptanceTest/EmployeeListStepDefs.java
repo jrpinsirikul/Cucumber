2bbe219744dbade626fb0b0c25e7f04bec3664b6
Johanna Pinsirikul <jpinsirikul@sevatec.com>
2016-09-07 13:12:35 -0400

    adding employee list cucumber tests

--- src/test/java/dhsFlashAcceptanceTest/DHSFlashAcceptanceTest/EmployeeListStepDefs.java
+++ src/test/java/dhsFlashAcceptanceTest/DHSFlashAcceptanceTest/EmployeeListStepDefs.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -23,34 +22,44 @@ import dhsFlashAcceptanceTest.Pages.GivePoint;
 public class EmployeeListStepDefs {
 	DesiredCapabilities capability;
 	WebDriver driver;
-	WebDriverWait waitDriver;	
-	
+	WebDriverWait waitDriver;
+	GivePoint pointPage;
+	String listTitle = "Latest Kudos";
+	String homeUrl;
+
 	@Given("^there is at least (\\d+) employee with a point in the system$")
 	public void there_is_at_least_employee_with_a_point_in_the_system(int arg1) throws Throwable {
 		capability = DesiredCapabilities.firefox();
 		capability.setBrowserName("firefox");
 		capability.setPlatform(Platform.LINUX);
-		
+
 		driver = new RemoteWebDriver(new URL("http://52.207.208.41:4444/wd/hub"),capability);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		//driver.get(formUrl);
-		
+
 		waitDriver = new WebDriverWait (driver, 15);
-		
-	    throw new PendingException();
 	}
 
-	@When("^I navigate to the Home screen$")
-	public void I_navigate_to_the_Home_screen() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+	@When("^I navigate to the Home screen (.+)$")
+	public void I_navigate_to_the_Home_screen(String homeUrl) throws Throwable {
+		driver.get(homeUrl);
+		pointPage = new GivePoint(driver);
+		waitDriver.until(ExpectedConditions.visibilityOf(pointPage.getKudosList()));
+		assertTrue(pointPage.getKudosList().isDisplayed());
+		assertTrue(pointPage.getList().isDisplayed());
+		assertEquals(listTitle, pointPage.getListTitleText());
 	}
 
 	@Then("^I will see a list of employees$")
 	public void I_will_see_a_list_of_employees() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-		driver.quit();
-	    throw new PendingException();
+		assertTrue(pointPage.getUsers().size() > 0);
 	}
 	
+	/**
+	 * Check if employee has points
+	 */
+	@And("^each employee with have their number of points listed with them$")
+	public void seeEmployeePoints() {
+		pointPage.getUsers().get(0);
+		driver.quit();
+	}
 }
--- src/test/resources/dhsFlashAcceptanceTest/DHSFlashAcceptanceTest/employeeList.feature
+++ src/test/resources/dhsFlashAcceptanceTest/DHSFlashAcceptanceTest/employeeList.feature
@@ -1,25 +1,17 @@
-#Author:tanusree.mccabe@sevatec.com
-#Keywords Summary :
-#Feature: List of scenarios.
-#Scenario: Business rule through list of steps with arguments.
-#Given: Some precondition step
-#When: Some key actions
-#Then: To observe outcomes or validation
-#And,But: To enumerate more Given,When,Then steps
-#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
-#Examples: Container for s table
-#Background: List of steps run before each of the scenarios
-#""" (Doc Strings)
-#| (Data Tables)
-#@ (Tags/Labels):To group Scenarios 
-#<> (placeholder)
-#""
-## (Comments)
-
-#Sample Feature Definition Template
-Feature: employeeList
-
-Scenario: employeeList
-Given there is at least 1 employee with a point in the system 
-When I navigate to the Home screen 
-Then I will see a list of employees
+#Feature: Employee point list
+#	As a user, I can list of employees with award points 
+#	and a corresponding award level
+#	
+#	Scenario: View employee point list
+#		Given there is at least 1 employee with a point in the system
+#		When I navigate to the Home screen
+#		Then I will see a list of employees
+Feature: View employee point list
+	Scenario Outline: Successfully view an employee list
+		Given there is at least 1 employee with a point in the system
+		When I navigate to the Home screen <form url>
+		Then I will see a list of employees
+			
+	Examples:
+		| form url |
+		| http://devint.linktecflash.com/DHSFlash/#/home |
