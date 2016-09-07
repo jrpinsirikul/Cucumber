b2f02d18ede70adc9a9d0bdd19c579a42ee70d84
tmccabe <tmccabe@E5470-9VMXL72.Sevatec.local>
2016-09-07 12:37:16 -0400

    employee with kudos list acceptance criteria

---
+++ src/test/java/dhsFlashAcceptanceTest/DHSFlashAcceptanceTest/employeeWithKudosListStepDefs.java
@@ -0,0 +1,21 @@
+package dhsFlashAcceptanceTest.DHSFlashAcceptanceTest;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class employeeWithKudosListStepDefs {
+
+	@Given("^there is greater than or equal to (\\d+) employee with a point in the system$")
+	public void there_is_greater_than_or_equal_to_employee_with_a_point_in_the_system(int arg1) throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^each employee with have their number of points listed with them$")
+	public void each_employee_with_have_their_number_of_points_listed_with_them() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+}
